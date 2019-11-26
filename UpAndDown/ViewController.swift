5546e0e785976b4dad565ff2ea742dd2936b1a1a
rhkwj <rhkwj@naver.com>
2019-11-26 19:53:03 +0800

    early commit

--- UpAndDown/ViewController.swift
+++ UpAndDown/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var inputNumberTextField: UITextField!
+    
+    @IBOutlet weak var gobutton: UIButton!
+    
+    @IBOutlet weak var hintLabel: UILabel!
+    
+    @IBOutlet weak var minimumLabel: UILabel!
+    
+    @IBOutlet weak var maximumLabel: UILabel!
+    
+    var finalNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // setup the game
+        finalNumber = Int(arc4random_uniform(50))
+        minimumLabel.text = String(1)
+        maximumLabel.text = String(50)
+        
+        // user can start to input the number
     }
-
-
+        
+    // 3. when user input the number, the inputNumberTextField changed
+    
+    // 4. when user press go, the "Up or Down" label changed, and "min" or "max" changed
+    @IBAction func goButtonPressed(_ sender: Any) {
+        print("goButtonPressed")
+    }
 }
-
